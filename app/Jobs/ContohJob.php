f91c64003c00825f80c92e1d08b347a312bc3840
lodaogos <leecharles565@gmail.com>
2023-11-07 10:11:52 +0700

    test

---
+++ app/Jobs/ContohJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ContohJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $tries = 5;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        
+        throw new \Exception('Failed!');
+    }
+
+    public function failed(\Throwable $e)
+    {
+        info('This job has failed');
+    }
+}
